8dafc36b1a7d10d1e3fc8baebfa24b183eb7bcbf
Josh King <king.jml@gmail.com>
2018-10-11 23:51:13 -0400

    IMU, LEDs and error codes
    - Added more status checking for the gps and radar.
    
    - Integrated the IMU to track the orientation of the radar
    
    - Added status LED and error codes

--- code.py
+++ code.py
@@ -1,21 +1,55 @@
 # ECCC Multi-application X-band Radar (ECMAXR)
 # J. King 2018
+# Controller: Adafruit Feather M4
+# Radar: SiversIMA RS3400X FMCW
+# GPS: Adafruit Ultimate GPS
+# IMU: Adafruit BNO055
 
+import neopixel
 import time
 import board
 import busio
-import adafruit_sdcard
 import storage
+import adafruit_sdcard
+import adafruit_bno055
+import gc
 from digitalio import DigitalInOut, Direction, Pull
+
+
+# Status LEDs
+pixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.5, auto_write=True)
+
+def update_status(verbose=False):
+    error_code = int(str(1) + str(radar.status) + str(imu.status)+ str(gps.status)+str(gps.fix))
+    
+    if verbose:
+        print('TRIG %i, RADAR %i, IMU %i, GPS %i, FIX %i' % (radar.count, 
+                radar.status, imu.status, gps.status, gps.fix))
+                
+    if error_code > 10000:
+        pixel.fill((255, 0, 0))
+        print('ERROR {}'.format(error_code))
+    elif error_code == 10001:
+        pixel.fill((255, 255, 0))
+        print('NO GPS FIX')
+    else:
+        pixel.fill((0, 255, 0))
+    
+    return error_code
+    
+    
 
 # GPS Class
 class GPS():
     def __init__(self, tx, rx):
-        self.uart = busio.UART(board.TX, board.RX, baudrate=9600, timeout=3000)
+        self.uart = busio.UART(tx, rx, baudrate=9600, timeout=3000)
         self.fix = 1
         self.status = 1
-        self.update()
-
+        self.last = None
+        self.update(debug = False)
+    
+    # Get GPS sentance and parse
+    # Will throw a 1 with timeout of 3000 ms
     def update(self, timeout=3000, debug=False):
         c_time = time.monotonic()
         while True:
@@ -34,10 +68,20 @@ class GPS():
                     self.lat = float(data[3])
                     self.lon = float(data[5])
                     if debug: print(data)
-                    break
+                    return 0
             elif l_time-c_time > timeout:
-                print('GPS Timeout')
-                break
+                self.status = 1
+                self.fix = 1
+                if debug: print('GPS Timeout')
+                return 1
+    
+    def print(self):
+        if self.fix == 0:
+            print('Timestamp: %s,%s' % (self.utc_date,self.utc_time))
+            print('Coordinates: %f,%f' %(self.lat, self.lon))
+        else:
+            print('NO FIX')
+
                 
 # Radar class
 class RADAR():
@@ -56,41 +100,50 @@ class RADAR():
                                receiver_buffer_size=64)
         self.count = -1
         self.trig = 0
-        self.last_trig = time.monotonic()
+        self.last_trig = -9999
            
         self.check()
         self.write(self.cmd['init'], True, False)
         self.write(self.cmd['measure'], True, False)
         self.write(self.cmd['sweep'], True, False)
-
+    
+    # Serial write to radar
     def write(self, cmd, read_now=False, debug=False):
         if debug: print(cmd)
         self.uart.reset_input_buffer()
         self.uart.write(cmd+'\r')
         if read_now:
             return self.read(debug)
-        
+    
+    # Read seial return from radar
     def read(self, debug=False):
         data = self.uart.readline()
         if debug: print(data)
         if ('OK' in data):
+            self.status = 0
             return 0
         elif data is None:
+            self.status = 1
             return 1
         else:
+            self.status = 0
             return data
-        
+            
+    # Radar heartbeat
     def check(self):
         self.write('')
         data = self.read()
         if '?' in data:
             self.status = 0
-            return True
+            return 0
         else:
             self.status = 1
-            return False
+            return 1
             
+    # Command the radar to fire and 
+    # read the data back over UART
     def fire(self):
+        #pixel.fill((255, 255, 0))
         self.uart.reset_input_buffer()
         counter = 0
         buf_len = 13 
@@ -99,8 +152,13 @@ class RADAR():
         idx = 0
         dat_out = []
         
+        # Send the trigger command to the radar
+        # Note radar.status gets updated by doing this
         self.write(self.cmd['trig'], True, False)
-        time.sleep(0.5)
+        
+        # Takes ~42 ms for the FMCW ramp to finish
+        time.sleep(0.5) 
+        
         self.uart.reset_input_buffer()
         if radar.status == 0:
             self.write(self.cmd['data'], False, False)
@@ -121,29 +179,33 @@ class RADAR():
         else:
             self.trig = 0
             return 1
+            
   
 # SD Card Init
+# TODO: Check for card presence 
+# and except gracefuly
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 cs = DigitalInOut(board.D10)
 sdcard = adafruit_sdcard.SDCard(spi, cs)
 vfs = storage.VfsFat(sdcard)
 storage.mount(vfs, "/sd")
+sd_error = 0
 
-# Init buttons
-radar_switch = DigitalInOut(board.D6)
-radar_switch.direction = Direction.INPUT
-radar_switch.pull = Pull.UP
-
-# Write to SD
+# This function constructs a JSON-like object
+# and stores it to the
 def write_data(data):
+    gc.collect()
+    error_code = update_status(True)
     out_str = str({'Id': radar.count, 
+                   'Error': error_code,
                    'Time': gps.utc_time,
                    'Date': gps.utc_date,
                    'Latitude': gps.lat,
                    'Longitude': gps.lon,
+                   'Quaternion': imu.quaternion,
+                   'Temperature': imu.temperature,
                    'Data': data}).replace("'", "\"")
-    
-    w_max = 1000
+    w_max = 1024
     w_remaining  = len(out_str)
             
     with open(file_name, 'a') as output:
@@ -156,22 +218,58 @@ def write_data(data):
         bytes_out = output.write(out_str[-w_remaining:])
     return 0
 
+# Switch Init
+radar_switch = DigitalInOut(board.D6)
+radar_switch.direction = Direction.INPUT
+radar_switch.pull = Pull.UP
+
+# IMU Init
+class IMU():
+    def __init__(self, i2c):
+        try:
+            self.sensor = adafruit_bno055.BNO055(i2c)
+        except:
+            self.status = 1
+        self.quaternion = None
+        self.temperature = None
+        time.sleep(0.5) #Needs a few ticks to wake
+        self.update()
+    
+    def update(self, verbose=False):
+        try:
+            self.quaternion = self.sensor.quaternion
+            self.temperature = self.sensor.temperature
+            self.status = 0
+            if verbose: self.print()
+        except:
+            self.status = 1
+            if verbose: print('IMU FAILED')
+    
+    def print(self):
+        print('Temperature: {}'.format(self.temperature))
+        print('Quaternion: {}'.format(self.quaternion))
+        
 
 # Main
+print('ECMAXR V0.1')
+i2c = busio.I2C(board.SCL, board.SDA)
+imu = IMU(i2c)
 gps = GPS(board.TX, board.RX)
-gps.update(debug=False)
-if (gps.status == 0): print('GPS READY')
-
-file_name = '/sd/%s_%s.rad' % (gps.utc_date, gps.utc_time)
-
 radar = RADAR(board.A2, board.A3)
-if (radar.status == 0): print('RADAR READY')
+    
+file_name = '/sd/%s_%s.dat' % (gps.utc_date, gps.utc_time)
+print('Writing to file: {}'.format(file_name))
+sys_status = update_status(True)
+if (sys_status == 10000): 
+    print('SYSTEM READY')
+else:
+    print('ERROR {}'.format(sys_status))
+
 
 while True:
     current = time.monotonic()
     if (not radar_switch.value) & (current-radar.last_trig > 1.0):
         radar.last_trig = time.monotonic()
-        print('RADAR TRIG %i' % radar.count)
-        gps.update(debug=False)
-        bytes_out = write_data(radar.fire())        
-        print('WROTE %i OK' % bytes_out)
+        gps.update(debug = False)
+        imu.update(verbose = False)
+        write_data(radar.fire())
